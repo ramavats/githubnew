63b1b5368dc4ea3c3cb567b95c18be7bd43063cb
suryadewa <suryadewa@users.noreply.github.com>
2016-06-22 00:43:31 +0800

    Update server.go

--- server/server.go
+++ server/server.go
@@ -14,7 +14,6 @@ import (
 	"time"
 
 	"github.com/jpillora/cloud-torrent/engine"
-	"github.com/suryadewa/ForNesiaToRrent/static"
 	"github.com/jpillora/go-realtime"
 	"github.com/jpillora/requestlog"
 	"github.com/jpillora/scraper/scraper"
